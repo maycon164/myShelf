b159486363bcb892bbe825bee1fa4ff687e21ecd
Maycon Huanca <maycon.huanca@e-core.com>
2022-08-29 07:54:51 -0300

    feat(books/games/series/movies): simple crud structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { SeriesModule } from './series/series.module';
+import { GamesModule } from './games/games.module';
+import { MoviesModule } from './movies/movies.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), BooksModule, SeriesModule, GamesModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
